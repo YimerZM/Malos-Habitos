a27c3ed91faaadbb16abd4a1f78e08d469790d5a
75065077 <75065077@continental.edu.pe>
2024-09-09 18:30:32 -0500

    Ejemplos001

---
+++ Ejemploo1.py
@@ -0,0 +1,15 @@
+
+def multiplicacion(multiplicando, multiplicador):
+    producto = multiplicando * multiplicador
+    return producto
+
+
+if __name__ == "__main__":
+    multiplicando = float(input("Multiplicando: "))
+    multiplicador = float(input("Multiplicador: "))
+    resultado = multiplicacion(multiplicando, multiplicador)
+    print(f"{multiplicando} * {multiplicador} = {resultado}")
+
+
+
+
